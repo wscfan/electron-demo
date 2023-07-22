c10811a4836e2cd18339457d49f7f1a2190eb80c
ws <wshappyday@sina.com>
2023-07-23 01:30:12 +0800

    搭建简单的摄像头样式

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const { BrowserWindow, app } = require("electron");
 const path = require("path");
 
-app.whenReady().then(() => {
+const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 300,
     height: 300,
     alwaysOnTop: true,
     x: 1180,
     y: 100,
+    frame: false,
+    transparent: true
+  });
+  mainWindow.setAspectRatio(1);
+  mainWindow.loadFile(path.resolve(__dirname, "index.html"));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  // mainWindow.webContents.toggleDevTools();
+
+  app.on("window-all-closed", () => {
+    if (process.platform != "darwin") app.quit();
+    console.log(333);
   });
 
-  mainWindow.webContents.toggleDevTools();
-  mainWindow.loadFile(path.resolve(__dirname, "index.html"));
+  app.on("activate", () => {
+    createWindow();
+  });
 });
